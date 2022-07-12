38bc996ea4096ee5f862f767d26c28bd878e3ab2
Dennis <year_2000_@hotmail.com>
2022-07-13 01:46:26 +0200

    finished burgemenu :))))

--- index.js
+++ index.js
@@ -36,12 +36,33 @@ function contact(event) {
   }
   
   
-  
-  
-  
   document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
      document.body.classList.remove('modal--open')
     }
   })
   
+
+
+
+  const menu_btn = document.querySelector('.menu__btn')
+  const times = document.querySelector('.times')
+  const hamburger = document.querySelector('.hamburger')
+  const placement = document.querySelector('.menu')
+  const menu__links = document.querySelector('.menu__links')
+
+  menu_btn.addEventListener("click", () => {
+    placement.classList.toggle('menu__active')
+    times.classList.toggle('times')
+    hamburger.classList.toggle('hamburger_inactive')
+
+
+  
+  })
+
+
+menu__links.addEventListener("click", () =>{
+  placement.classList.toggle('menu__active')
+  times.classList.toggle('times')
+  hamburger.classList.toggle('hamburger_inactive')
+})
